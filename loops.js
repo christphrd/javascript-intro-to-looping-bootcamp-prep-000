f36b0ae0778762d88dd37f53e85e39a2638ba3e1
Christopher Diep <diep.christopher@gmail.com>
2017-10-07 22:24:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,18 @@
-function forLoop (var i=0; i<25; i++) {
-  "I am" + i + "strange loop"
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if ( i == 1) {
+      array.push("I am 1 strange loop.")
+    };
+    else {
+      array.push("I am ${i} strange loops.")
+    };
+  return array
+}
+
+function whileLoop(n) {
+  while(n>0) {
+    console.log(n)
+    --n
+  }
+  return 'done'
 }
